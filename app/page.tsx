d115d9dc1d6177a5c2c84a8e98ed55f38d648270
JerryBoi <osagiededivine123@gmail.com>
2026-09-04 03:57:45 +0100

    feat: add home page title

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import type { Metadata } from "next";
 import { HomeExperience } from "@/components/home-experience";
+
+export const metadata: Metadata = { title: "Home" };
 
 export default function HomePage() {
   return <HomeExperience />;
